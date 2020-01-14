5d3bed049312806e39426f3ddfc26fafcc5061d8
Robert Collins <rpc219@gmail.com>
2020-01-14 23:15:12 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,58 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague => {
+       :patriarch => {
+         :name => "Lord Montague",
+         :age => "53"
+       },
+       :matriarch => {
+         :name => "Lady Montague",
+         :age => "54"
+       },
+       :hero => {
+         :name => "Romeo",
+         :age => "15",
+         :status => "alive"
+       },
+       :hero_friends => [
+         {
+           :name => "Benvolio",
+           :age => "17",
+           :attributes => "worried"
+         },
+         {
+           :name => "Mercutio",
+           :age => "18",
+           :attributes => "hot-headed"
+         }
+       ]
+     },
+    :capulet => {
+      :patriarch => {
+        :name => "Lord Capulet",
+        :age => "50",
+      },
+      :matriarch => {
+        :name => "Lady Capulet",
+        :age => "51"
+      },
+      :heroine => {
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
+      },
+      :heroine_friends => [
+        {
+          :name => "Steven",
+          :age => "30",
+          :attributes => "confused"
+        },
+        {
+          :name => "Nurse",
+          :age => "44",
+          :attributes => "worried"
+        }
+      ]
+    }
+   }
 end
